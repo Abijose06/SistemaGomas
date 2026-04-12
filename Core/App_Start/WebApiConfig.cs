482b118653027f6d5e660a19ffb6d5bb8ec4bf4d
Abijose06 <abijosemarinez@gmail.com>
2026-04-12 14:23:39 -0400

    CORS

--- Core/App_Start/WebApiConfig.cs
+++ Core/App_Start/WebApiConfig.cs
@@ -10,6 +10,8 @@ namespace Core
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
+            var cors = new System.Web.Http.Cors.EnableCorsAttribute("*", "*", "*");
+            config.EnableCors(cors);
 
             // Web API routes
             config.MapHttpAttributeRoutes();
